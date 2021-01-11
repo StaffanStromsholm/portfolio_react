542803acbf51c31c975c0ff76ba2d252913c6dc3
Staffan Strömsholm <staffan.stromsholm@gmail.com>
2021-01-11 22:41:27 +0200

    Animation.js now works, changed hero-banner styling

--- src/Components/Animation/Animation.js
+++ src/Components/Animation/Animation.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import cogwheel from './1295308.svg';
 
 const Animation = () => {
-    return (<section class="cogwheels">
-        <div class="cogwheel-1"><img src="/public/cogwheel.svg" alt="cogwheel" /></div>
-        <div class="cogwheel-2"><img src="././public/cogwheel.svg" alt="cogwheel" /></div>
-        <div class="cogwheel-3"><img src="/public/cogwheel.svg" alt="cogwheel" /></div>
-        <div class="cogwheel-4"><img src="/public/cogwheel.svg" alt="cogwheel" /></div>
-        <div class="cogwheel-5"><img src="/public/cogwheel.svg" alt="cogwheel" /></div>
+    return (<div id="animation"><section class="cogwheels">
+        <div class="cogwheel-1"><img src={cogwheel} alt="cogwheel" /></div>
+        <div class="cogwheel-2"><img src={cogwheel} alt="cogwheel" /></div>
+        <div class="cogwheel-3"><img src={cogwheel} alt="cogwheel" /></div>
+        <div class="cogwheel-4"><img src={cogwheel} alt="cogwheel" /></div>
+        <div class="cogwheel-5"><img src={cogwheel} alt="cogwheel" /></div>
     </section>
+    </div>
 
     )
 }
