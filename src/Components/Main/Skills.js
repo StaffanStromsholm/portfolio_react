b77450c1d9e052324160383add5046387f18f18a
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-02-20 13:36:50 +0200

    updated skill points

--- src/Components/Main/Skills.js
+++ src/Components/Main/Skills.js
@@ -12,52 +12,52 @@ const Skills = () => {
                     <div class="progress-group">
                         <label for="file"><i class="fab fa-html5"></i></label>
                         <p>HTML</p>
-                        <progress id="file" value="30" max="100"> 50% </progress>
+                        <progress id="file" value="50" max="100"> 50% </progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fab fa-css3-alt"></i></label>
                         <p>CSS</p>
-                        <progress id="file" value="30" max="100"></progress>
+                        <progress id="file" value="50" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fab fa-js-square"></i></label>
                         <p>Javascript</p>
-                        <progress id="file" value="30" max="100"></progress>
+                        <progress id="file" value="50" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fab fa-node"></i></label>
                         <p>NodeJS</p>
-                        <progress id="file" value="20" max="100"></progress>
+                        <progress id="file" value="40" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fab fa-spin fa-react"></i></label>
                         <p>React</p>
-                        <progress id="file" value="5" max="100"></progress>
+                        <progress id="file" value="50" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fas fa-mug-hot"></i></label>
                         <p>Redux</p>
-                        <progress id="file" value="5" max="100"></progress>
+                        <progress id="file" value="30" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i><img src={TSLogo} class="typescript-logo"></img></i></label>
                         <p>Typescript</p>
-                        <progress id="file" value="5" max="100"></progress>
+                        <progress id="file" value="20" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i><img src={mongoLogo} class="mongodb-logo"></img></i></label>
                         <p>MongoDB</p>
-                        <progress id="file" value="20" max="100"></progress>
+                        <progress id="file" value="30" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i><img src={SQLLogo} class="mongodb-logo"></img></i></label>
                         <p>SQL</p>
-                        <progress id="file" value="20" max="100"></progress>
+                        <progress id="file" value="30" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fab fa-git-square"></i></label>
                         <p>Git</p>
-                        <progress id="file" value="20" max="100"></progress>
+                        <progress id="file" value="50" max="100"></progress>
                     </div>
                     <div class="progress-group">
                         <label for="file"><i class="fas fa-medal"></i></label>
