62c1ecf97b8e1213b93ef89047eb71a8842dab2e
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-05-16 14:08:54 +0300

    try again, build failed last time

--- src/Components/Main/Projects.js
+++ src/Components/Main/Projects.js
@@ -14,37 +14,37 @@ function Projects() {
     return (
         <div id="projects">
             <div className="projects-title">My projects </div>
-        <Carousel className="projects">
-        <div className="project-info">
-            
-                <div className="project-img"><img src="https://lh3.googleusercontent.com/C42_0chM8PKfSSfXPnFqEOuy1jgDBEwVIwWjI8wkF_hapbNTOqY6BV1pDzXVrdEjQ0YVMIKZTebmnwb4hLyddJhKtSWxSojbNzpCH9CvYyXH9v6aaCJ8iLUSJIukkIBkd_TGcD7KEw=w2400"></img></div>
-            <h4 className="project-title">Bandmate Finder</h4>
-            <p className="project-description">A social media app created for musicians. Users can create profiles, and connect with other users by commenting on their profiles. The users address is geocoded on the backend when signing up based on city and postal code, and the coordinates are stored to MongoDB along with the other information. You can filter users on the map so only users playing a certain instrument are shown. Technologies: React, Node, Express, mongoDB, Here Maps, Bcrypt for salting and hashing passwords, Node geocoder.</p>
-            <ul>
-                <li className="project-link"><a href="https://bandmatefinderclient.herokuapp.com/BandmateFinder-client"><i class=" fas fa-chevron-right"></i> Live</a></li>
-                <li className="project-link"><a href="https://github.com/StaffanStromsholm/BandmateFinder-client"><i class="fas fa-chevron-right"></i> GitHub</a></li>
-            </ul>
-            </div>
-             
-            {posts.slice(1).map(post =>
-            <div className="project-info">
-                <div className="project-img"><img src={post.imgURL}></img></div>
-               
-            <h4 className="project-title">{post.title}</h4>
-            <p className="project-description">{post.description}</p>
-            <ul>
-                {post.liveURL && <li className="project-link"><a href={post.liveURL}><i class=" fas fa-chevron-right"></i> Live</a></li>}
-                {post.githubURL && <li className="project-link"><a href={post.githubURL}><i class="fas fa-chevron-right"></i> GitHub</a></li>}
-            </ul>
-            </div>
-            )}
-   
-        </Carousel>
+            <Carousel className="projects">
+                <div className="project-info">
+
+                    <div className="project-img"><img src="https://lh3.googleusercontent.com/C42_0chM8PKfSSfXPnFqEOuy1jgDBEwVIwWjI8wkF_hapbNTOqY6BV1pDzXVrdEjQ0YVMIKZTebmnwb4hLyddJhKtSWxSojbNzpCH9CvYyXH9v6aaCJ8iLUSJIukkIBkd_TGcD7KEw=w2400"></img></div>
+                    <h4 className="project-title">Bandmate Finder</h4>
+                    <p className="project-description">A social media app created for musicians. Users can create profiles, and connect with other users by commenting on their profiles. The users address is geocoded on the backend when signing up based on city and postal code, and the coordinates are stored to MongoDB along with the other information. You can filter users on the map so only users playing a certain instrument are shown. Technologies: React, Node, Express, mongoDB, Here Maps, Bcrypt for salting and hashing passwords, Node geocoder.</p>
+                    <ul>
+                        <li className="project-link"><a href="https://bandmatefinderclient.herokuapp.com/BandmateFinder-client"><i class=" fas fa-chevron-right"></i> Live</a></li>
+                        <li className="project-link"><a href="https://github.com/StaffanStromsholm/BandmateFinder-client"><i class="fas fa-chevron-right"></i> GitHub</a></li>
+                    </ul>
+                </div>
+
+                {posts.slice(1).map(post =>
+                    <div className="project-info">
+                        <div className="project-img"><img src={post.imgURL}></img></div>
+
+                        <h4 className="project-title">{post.title}</h4>
+                        <p className="project-description">{post.description}</p>
+                        <ul>
+                            {post.liveURL && <li className="project-link"><a href={post.liveURL}><i class=" fas fa-chevron-right"></i> Live</a></li>}
+                            {post.githubURL && <li className="project-link"><a href={post.githubURL}><i class="fas fa-chevron-right"></i> GitHub</a></li>}
+                        </ul>
+                    </div>
+                )}
+
+            </Carousel>
         </div>
-           
-                    
-        
-                );
+
+
+
+    );
 };
 
 export default Projects;
--- src/Components/Main/Skills.js
+++ src/Components/Main/Skills.js
@@ -12,42 +12,42 @@ const Skills = () => {
                     <div className="progress-group">
                         <label for="file"><i className="fab fa-html5"></i></label>
                         <p>HTML</p>
-                        <progress id="file" value="50" max="100"> 50% </progress>
+                        <progress id="file" value="80" max="100"> 50% </progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i className="fab fa-css3-alt"></i></label>
                         <p>CSS</p>
-                        <progress id="file" value="50" max="100"></progress>
+                        <progress id="file" value="80" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i className="fab fa-js-square"></i></label>
                         <p>Javascript</p>
-                        <progress id="file" value="50" max="100"></progress>
+                        <progress id="file" value="70" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i className="fab fa-node"></i></label>
                         <p>NodeJS</p>
-                        <progress id="file" value="40" max="100"></progress>
+                        <progress id="file" value="60" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i className="fab fa-spin fa-react"></i></label>
                         <p>React</p>
-                        <progress id="file" value="50" max="100"></progress>
+                        <progress id="file" value="70" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i className="fas fa-mug-hot"></i></label>
                         <p>Redux</p>
-                        <progress id="file" value="30" max="100"></progress>
+                        <progress id="file" value="50" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i><img src={TSLogo} className="typescript-logo"></img></i></label>
                         <p>Typescript</p>
-                        <progress id="file" value="20" max="100"></progress>
+                        <progress id="file" value="40" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i><img src={mongoLogo} className="mongodb-logo"></img></i></label>
                         <p>MongoDB</p>
-                        <progress id="file" value="30" max="100"></progress>
+                        <progress id="file" value="50" max="100"></progress>
                     </div>
                     <div className="progress-group">
                         <label for="file"><i><img src={SQLLogo} className="mongodb-logo"></img></i></label>
@@ -57,7 +57,7 @@ const Skills = () => {
                     <div className="progress-group">
                         <label for="file"><i className="fab fa-git-square"></i></label>
                         <p>Git</p>
-                        <progress id="file" value="50" max="100"></progress>
+                        <progress id="file" value="70" max="100"></progress>
                     </div>
                     {/* <div className="progress-group">
                         <label for="file"><i className="fas fa-medal"></i></label>
